5c0a6379ad416bcf22de78594da0cdf81c04fd67
AlanTapia <44512347+soyalantapia@users.noreply.github.com>
2022-03-03 18:55:29 -0300

    cambios

--- validators/Users.js
+++ validators/Users.js
@@ -6,11 +6,11 @@ const { validateResult } = require('../helpers/validateHelper')
 const validateCreate = [
     //validacion
     body('email', 'Ingrese un email valido')
-    .exists() .isEmail() .normalizeEmail(),
+    .isEmail() .normalizeEmail(),
     body('name','Ingrese un nombre de usuario valido')
-        .exists() .isLength({max:20}) .isLength({min:5}) .not().isEmpty().trim().escape(),
+       .isLength({max:20}) .isLength({min:5}) .not() .isEmpty() .trim() .escape(),
     body('message','Ingrese una motivacion valida')
-        .exists() .isLength({max:200}) .isLength({min:10}) .not().isEmpty().trim().escape(),
+     .isLength({max:200}) .isLength({min:10}) .not(). isEmpty() .trim() .escape(),
    /*  body('wallet', 'Ingrese un Wallet valido')   
         .exists() .isLength({min:5}) .not() .isEmpty() .trim() .escape() .blacklist(), */
         (req, res, next) => {
