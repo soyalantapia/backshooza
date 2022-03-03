4f619faeb1f45ba94907b98e1fe2d95e93ec44b0
AlanTapia <44512347+soyalantapia@users.noreply.github.com>
2022-03-03 19:02:00 -0300

    Unico al usuario

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Shema = mongoose.Shema;
 //Creando el modulo de la base de datos
 const UserSchema = new mongoose.Schema({
     email: {type:String,  required:true, unique:true},
-    name: {type:String, required:true},
+    name: {type:String, required:true, unique:true},
     message: {type:String, required:true},
     //wallet: {type:String},
 });
