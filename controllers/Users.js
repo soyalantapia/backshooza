99479b9f14b907533f3f3ab5fa7c3aa99f6c8ae2
AlanTapia <44512347+soyalantapia@users.noreply.github.com>
2022-03-15 18:47:15 -0300

    agregando recapchat

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const User = require('../models/User')
 const recaptcha = require('../controllers/recaptcha'); 
-var request = require('request');
+const request = require('request');
 const { exists } = require('../models/User');
 
 
@@ -29,7 +29,24 @@ const findById = (req,res) =>{
 /* --------- AGREGAR UN USUARIO -------- */
 const addUser = (req,res) =>{
 
+    //Si no se manda los datos desde el front lanza error de "algo esta saliendo mal"
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
+  {
+    return res.json({"responseError" : "Algo esta saliendo mal"});
+  }
+  const secretKey = process.env.SECRET_KEY_RECAPTCHA;
   
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+
+    //Si la conexion funciona mal lanza el siguiente error
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseError" : "Error Recaptcha, fracaso validacion"});
+    }else{
+    let respuesta = true
+    if(respuesta === true ){ 
+    
         let user = new User({ 
             email: req.body.email,
             name: req.body.name,
@@ -37,32 +54,38 @@ const addUser = (req,res) =>{
           })
          user.save((err,usr)=>{
              err && res.status(500).send(err.message);
-             res.status(200).json({usr});
+             res.status(200).json({"responseSuccess" : "recaptcha exito", usr});
          })      
- 
+         };
+
+         
+    } //Cierre del if
+
     /* -------- AGREGAR UN USUARIO -> CONEXION CON MAILERLITE -------- */
-    var options = {
-      'method': 'POST',
-      'url': 'https://api.mailerlite.com/api/v2/subscribers',
-      'headers': {
-        'X-MailerLite-ApiKey': '3df4346d696c3ef5510ab772f2ebb85f',
-        'Content-Type': 'application/json',
-        'Cookie': 'PHPSESSID=3ba7202d14ff5c93df070cfddde703d9'
-      },
+         var request = require('request');
+          var options = {
+            'method': 'POST',
+            'url': 'https://api.mailerlite.com/api/v2/subscribers',
+            'headers': {
+              'X-MailerLite-ApiKey': '3df4346d696c3ef5510ab772f2ebb85f',
+              'Content-Type': 'application/json',
+              'Cookie': 'PHPSESSID=3ba7202d14ff5c93df070cfddde703d9'
+            },
 
-      body: JSON.stringify({
-        "email": req.body.email,
-        "resubscribe": false,
-        "type": "active",
-        "name": req.body.name
-      })
+            body: JSON.stringify({
+              "email": req.body.email,
+              "resubscribe": false,
+              "type": "active",
+              "name": req.body.name
+            })
 
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      console.log(response.body);
-    });
-    //Fin de conexion con mailerlite
+          };
+          request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+          });
+          //Fin de conexion con mailerlite
+});
 
 };
 
